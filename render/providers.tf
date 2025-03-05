f2733ec1736b1d15229e8e8e095a31e85938d1e9
= <=>
2025-03-05 13:18:53 -0500

    End of practicum4

---
+++ render/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    render = {
+      source = "render-oss/render"
+      version = "1.5.0"
+    }
+  }
+}
+
+
+provider "render" {
+  # Configuration options
+
+}
