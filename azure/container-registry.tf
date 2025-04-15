bf9dfc07fdf56b861a7d10425d468f12ad36127e
= <=>
2025-04-15 16:44:27 -0400

    End of practicum9

--- azure/container-registry.tf
+++ azure/container-registry.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_container_registry" "container_registry" {
-  name = "practicum8"
+  name = "practicum10"
   resource_group_name = azurerm_resource_group.p466.name
   location = var.location
   admin_enabled = true
